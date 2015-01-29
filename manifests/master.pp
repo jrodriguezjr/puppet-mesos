7edceab7e921bdf91b573499980b9cba4e56f77e
Joe Rodriguez Jr <jrod@rackspace.com>
2015-01-29 10:46:48 -0600

    Testing again

--- manifests/master.pp
+++ manifests/master.pp
@@ -25,7 +25,7 @@ class mesos::master(
   $env_var        = {},
   $options        = {},
   $force_provider = undef, #temporary workaround for starting services
-  repo            = undef,
+  $repo            = undef,
 ) inherits mesos {
 
   validate_hash($env_var)
